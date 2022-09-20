58053511d6be2af646a611e009aa92492d8b0109
Abhishek Singh <avsekjob@gmail.com>
2022-09-20 16:43:04 +0530

    .

---
+++ numpyPractice.py
@@ -0,0 +1,19 @@
+# Python program for
+# Creation of Arrays
+import numpy as np
+
+# Creating a rank 1 Array
+arr = np.array(([1, 2, 3], [4,5,6]))
+a = np.array([4,5,6])
+print("Array with Rank 1: \n", arr[0])
+print("Array with Rank 1.1: \n", a)
+
+# Creating a rank 2 Array
+arr = np.array([[1, 2, 3],
+                [4, 5, 6]])
+print("Array with Rank 2: \n", arr)
+
+# Creating an array from tuple
+arr = np.array((1, 3, 2))
+print("\nArray created using "
+      "passed tuple:\n", arr)
